874b51449111ec002d13ff449cedffa2dd697bd4
hdser <hugo.serodio@gnosis.io>
2025-04-29 15:25:17 +0200

    add new model

---
+++ src/queries/historical_probelab_cl_client_quic_distribution.js
@@ -0,0 +1,14 @@
+const metric = {
+  id: 'historical_probelab_cl_client_quic_distribution',
+  name: 'Probelab: CL Client QUIC Support Distribution',
+  description: 'Distribution of consensus client QUIC Support (7DMA)',
+  format: 'formatNumber',
+  chartType: 'stackedBar',
+  labelField: 'client', 
+  subLabelField: 'quic', 
+  valueField: 'value',
+  enableFiltering: true,
+  query: `SELECT * FROM dbt.probelab_peers_clients_quic_daily ORDER BY date ASC, client ASC, quic ASC`,
+};
+
+export default metric;
