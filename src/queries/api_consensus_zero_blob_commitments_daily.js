a98d4fbc18d6a82a310a79ca5e070035c1c653b0
hdser <hugo.serodio@gnosis.io>
2025-10-28 22:39:29 +0100

    new metric

---
+++ src/queries/api_consensus_zero_blob_commitments_daily.js
@@ -0,0 +1,32 @@
+const metric = {
+  id: 'api_consensus_zero_blob_commitments_daily',
+  name: 'Blocks With Blob Commitments',
+  description: 'Daily produced blocks with/without blobs',
+  chartType: 'area', 
+  format: 'formatNumber',
+  isTimeSeries: true,
+  enableZoom: true,
+  showTotal: true, 
+  
+  // Bar chart styling
+  barWidth: 'auto',
+  barMaxWidth: 50,
+  borderRadius: [1, 1, 0, 0],
+
+  symbolSize: 2,
+  lineWidth: 2,
+  barOpacity: 0.8,  
+  
+  defaultZoom: {
+    start: 0, 
+    end: 100   
+  },
+
+  xField: 'date',
+  yField: 'value',
+  seriesField: 'label',
+
+  query: `SELECT * FROM dbt.api_consensus_zero_blob_commitments_daily`,
+};
+
+export default metric;
