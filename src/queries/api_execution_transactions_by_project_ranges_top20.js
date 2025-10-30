41e1cad1bd434b1df584bb151a0dd34c6d7e1b8c
mlaegn <maximilian.laechelin@gnosis.io>
2025-10-30 17:43:52 +0000

    add range dropdown to pie charts, weekly instead of daily

---
+++ src/queries/api_execution_transactions_by_project_ranges_top20.js
@@ -0,0 +1,19 @@
+const metric = {
+  id: 'api_execution_transactions_by_project_ranges_top20',
+  name: 'Transactions by Project',
+  description: 'Select range',
+  chartType: 'pie',
+  nameField: 'label',
+  valueField: 'value',
+  enableFiltering: true,
+  labelField: 'range',
+  format: 'formatNumber',
+  legend: { top: 'top', left: 'center', type: 'scroll', itemGap: 16 },
+  donut: true,
+  showLabels: true,
+  labelFormatter: '{b}: {c} ({d}%)',
+  sortByValue: 'desc',    
+  query: `SELECT * FROM dbt.api_execution_transactions_by_project_ranges_top20`,
+};
+
+export default metric;
